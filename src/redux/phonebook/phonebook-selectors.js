4554880018e8d114a50cf4eba6a18ca15de784d2
Eugenia-gal <galevbor12@gmail.com>
2021-08-29 13:30:25 +0300

    add memoization

--- src/redux/phonebook/phonebook-selectors.js
+++ src/redux/phonebook/phonebook-selectors.js
@@ -1,13 +1,16 @@
+import { createSelector } from 'reselect';
+
 export const getItems = state => state.phonebook.contacts.items;
 export const getFilter = state => state.phonebook.filter;
 export const getStatus = state => state.phonebook.contacts.status;
 export const getError = state => state.phonebook.contacts.error;
 
-export const getVisibleContacts = state => {
-  const items = getItems(state);
-  const filter = getFilter(state);
-  const normalizedFilter = filter.toLowerCase();
-  return items.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter),
-  );
-};
+export const getVisibleContacts = createSelector(
+  [getItems, getFilter],
+  (items, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return items.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  },
+);
